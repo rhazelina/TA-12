600dbcd87b724274f3aa9deef0c6684fbb1e7a13
iqbalkahini <iqbal.lazuardi07@gmail.com>
2026-02-06 09:37:24 +0700

    memperbaiki lebar halaman edit peserta pada role koordinator

--- src/app/koordinator/peserta/edit/[id]/page.tsx
+++ src/app/koordinator/peserta/edit/[id]/page.tsx
@@ -216,7 +216,7 @@ export default function EditSiswaPage() {
     }
 
     return (
-        <div className="space-y-6 max-w-4xl mx-auto">
+        <div className="space-y-6 mx-10">
             {/* Header */}
             <div className="flex items-center justify-between">
                 <div className="flex items-center space-x-4">
