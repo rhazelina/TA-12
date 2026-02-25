f66515af9d65dfcf385a216d631a2b028f2477c7
iqbalkahini <iqbal.lazuardi07@gmail.com>
2026-02-25 13:41:45 +0700

    Update page.tsx

--- src/app/kapro/pengajuan-pkl/page.tsx
+++ src/app/kapro/pengajuan-pkl/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import React from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import PengajuanIndividu from "@/components/kapro/pengajuan/individu";
 import PengajuanKelompok from "@/components/kapro/pengajuan/kelompok";
